e25c3fc07e82102b9db51d0d1b440c589a92a9d2
Anne C. Yates <annecreeyates@gmail.com>
2023-08-15 14:43:19 +0100

    HTML: added the stylesheet, changed the name of the document, changed the text in csv-file-loader paragraph to simple instructions, changed the preview button to excel preview; Styles.css: added basic properties to csv-preview class to make reading easier; Main.js: added required columns with their indexes.

--- main.js
+++ main.js
@@ -17,11 +17,16 @@ document.getElementById("upload-file_button").addEventListener(
         const csv = event.target.result;
         const data = csv.split("\n").map((row) => row.split(","));
         const data_without_first_two_rows = data.slice(2);
-        //column names start with the row 3 in the csv file
+        //column names start with the row 3 in the csv file -- at the moment column_names is not used
         const column_names = data[2];
+        // indexes of the columns that need to be presented
+        const requiredColumns = [1, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14];
+
 
         //table
         const table = document.getElementById("loaded_csv_data_table");
+        // create the table using the data slice
+        // print out only the data that is included in the requiredColumns -- includes index
         data_without_first_two_rows.forEach((row) => {
           const table_row = document.createElement("tr");
           row.forEach((content) => {
