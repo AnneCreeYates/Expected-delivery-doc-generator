cb4a83b1ea2080e1e96072495f57f1b1b302d6d4
Anne C. Yates <annecreeyates@gmail.com>
2023-08-16 13:32:02 +0100

    In html: added a div separating the table from the rest of the elements in the section; in css: added basic style to the table-container and loaded data table; in js: unified to camelCase, added the requiredColNames and introduced a function that programaticelly checks for the required columns content and indexes, which then are used in maping and filtering the required data and printed to the browser.

--- main.js
+++ main.js
@@ -16,26 +16,44 @@ document.getElementById("upload-file_button").addEventListener(
       reader.onload = (event) => {
         const csv = event.target.result;
         const data = csv.split("\n").map((row) => row.split(","));
-        const data_without_first_two_rows = data.slice(2);
-        //column names start with the row 3 in the csv file -- at the moment column_names is not used
-        const column_names = data[2];
-        // indexes of the columns that need to be presented
-        const requiredColumns = [1, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14];
-
-
-        //table
+        //column names start with the row 3 in the csv file
+        const columnNames = data[2];        
+        const requiredColNames = ["Pre-Advice ID", "Status", "Due Date", "Rec'd Date", "SKU", "SKU Description", "Qty Ord'd", "Qty Rec'd", "Supp ID", "Supplier Name", "Shelf Life ", "Shelf Life "]
+        const dataWithoutFirstTwoRows = data.slice(2);
+        
+            
+        // rendering the table
         const table = document.getElementById("loaded_csv_data_table");
-        // create the table using the data slice
-        // print out only the data that is included in the requiredColumns -- includes index
-        data_without_first_two_rows.forEach((row) => {
-          const table_row = document.createElement("tr");
-          row.forEach((content) => {
-            const table_data = document.createElement("td");
-            table_data.textContent = content;
-            table_row.appendChild(table_data);
-          });
-          table.appendChild(table_row);
-        });
+            // create the table using the columnNames data slice
+            // print out only the data that is included in the requiredColumns -- includes index
+
+            //CHANGE THAT INTO A SEPAREATE FUNCTION ---
+            // indexes of the columns that need to be presented
+            let requiredColumnIndexes =  columnNames.map((columnName, index) => requiredColNames.includes(columnName) ? index : -1).filter(index => index !== -1);
+            
+            // use the slice method to get only the data rows (without the title row)
+            let requiredDataRows = dataWithoutFirstTwoRows.slice(1);
+
+            // use the map method to create a new array with only the required columns
+            let filteredData = requiredDataRows.map(row => {
+              // use the filter method to get oinly the required columns
+              let newRow = row.filter((value, index) => requiredColumnIndexes.includes(index));
+              return newRow;
+            })
+
+            // ---
+            
+          // this version of the loop prints everything to the browser -- change that into a separate function ----
+            filteredData.forEach((row) => {
+              const table_row = document.createElement("tr");
+              row.forEach((content) => {
+                const table_data = document.createElement("td");
+                table_data.textContent = content;
+                table_row.appendChild(table_data);
+              });
+              table.appendChild(table_row);
+            });
+            // ----
       };
     }
   },
